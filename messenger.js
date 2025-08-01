5d5450df089177304c287c921f7faed71c1d556a
humayundanish05 <humayundanish50@gmail.com>
2025-08-02 03:29:21 +0500

    Update messenger.js

--- messenger.js
+++ messenger.js
@@ -9,7 +9,7 @@ const nameInput = document.getElementById("newChatName");
 //pick random avatar for new generated chats 
 
 
-const avatar = [
+const avatarURLs = [
   "https://i.pravatar.cc/150?img=1", "https://i.pravatar.cc/150?img=2", "https://i.pravatar.cc/150?img=3", "https://i.pravatar.cc/150?img=4",
   "https://i.pravatar.cc/150?img=5", "https://i.pravatar.cc/150?img=6", "https://i.pravatar.cc/150?img=7", "https://i.pravatar.cc/150?img=8",
   "https://i.pravatar.cc/150?img=9", "https://i.pravatar.cc/150?img=10", "https://i.pravatar.cc/150?img=11", "https://i.pravatar.cc/150?img=12",
@@ -269,6 +269,7 @@ function sendFakeReply() {
     messages.scrollTop = messages.scrollHeight;
   }, 1500);
 }
+
 
 
 
