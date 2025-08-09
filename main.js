f4f521fb3776479cf8ae8e5eae1780a0eaebf354
humayundanish05 <humayundanish50@gmail.com>
2025-08-09 12:09:39 +0500

    Update main.js

--- main.js
+++ main.js
@@ -138,8 +138,8 @@ localStorage.setItem("posts", JSON.stringify(posts));
 function formatTime(isoString) {
 const seconds = Math.floor((Date.now() - new Date(isoString)) / 1000);
 if (seconds < 60) return "Just now";
-if (seconds < 3600) return ${Math.floor(seconds / 60)}m ago;
-if (seconds < 86400) return ${Math.floor(seconds / 3600)}h ago;
+if (seconds < 3600) return `${Math.floor(seconds / 60)}m ago`;
+if (seconds < 86400) return `${Math.floor(seconds / 3600)}h ago`;
 return new Date(isoString).toLocaleDateString();
 }
 });
